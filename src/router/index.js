094e595103e9274d3b94dc59921761dce0cf52e7
Kazi Omar Faruk <kaziomar144@gmail.com>
2024-08-08 15:11:00 +0600

    redirecting and catch all routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TeamsList from '@/components/teams/TeamsList.vue'
 import UsersList from '@/components/users/UsersList.vue'
 import TeamMembers from '@/components/teams/TeamMembers.vue'
+import NotFound from '@/components/nav/NotFound.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      // name: 'teams-list',
+      // component: TeamsList
+      redirect: '/teams'
+    },
+    {
       path: '/teams',
       name: 'teams-list',
       component: TeamsList
+      // alias: '/'
     },
-    {
-      path: '/users',
-      name: 'users-list',
-      component: UsersList
-    },
-    {
-      path: '/teams/:teamId',
-      component: TeamMembers,
-      props: true
-    }
+    { path: '/users', name: 'users-list', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 })
